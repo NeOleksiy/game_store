99b3fc73c648f39aeb426010ab321cd02242f388
Ne_Oleksiy <alyefim@gmail.com>
2023-03-06 01:46:58 +0300

    create models

--- games/admin.py
+++ games/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from games.models import Category, Products
 # Register your models here.
+
+
+
+admin.site.register(Products)
+admin.site.register(Category)
--- games/models.py
+++ games/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    category = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.category
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=128)
+    purchase_method = models.CharField(max_length=64)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    account_details = models.TextField(null=True, blank=True)
+    rental_time = models.DateField(null=True, blank=True)
+    key = models.CharField(max_length=64)
+    description = models.TextField()
+    image = models.ImageField(upload_to='games_images')
+    category = models.ForeignKey(to=Category, on_delete=models.CASCADE)
+
